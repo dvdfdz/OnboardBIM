98f8e11eafab82fe8be0ed36d50e727df0fee564
dvdfdz <dvdfdz@gmail.com>
2025-05-28 21:06:31 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -57,26 +57,19 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 
-let slideIndex = [1,1];
-/* Class the members of each slideshow group with different CSS classes */
-let slideId = ["mySlides1", "mySlides2"]
-showSlides(1, 0);
-showSlides(1, 1);
+let slideIndex = 0;
+showSlides();
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
-}
-
-function showSlides(n, no) {
+function showSlides() {
   let i;
-  let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  let slides = document.getElementsByClassName("mySlides1");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";
-  setTimeout(showSlides, 2500); // Change image every 2 seconds
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
 
