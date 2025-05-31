4a30ba3d5cd29d91ec694b065d1b1d012e92e6e5
dvdfdz <dvdfdz@gmail.com>
2025-05-31 15:00:30 +0200

    edited

--- js/scripts.js
+++ js/scripts.js
@@ -51,9 +51,29 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    // Activate SimpleLightbox plugin for portfolio items
+    /* Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
         elements: '#portfolio a.portfolio-box'
-    });
+    });*/
 
+    let slideIndex = [1, 1];
+    /* Class the members of each slideshow group with different CSS classes */
+    let slideId = ["mySlides1", "mySlides2"]
+    showSlides(1, 0);
+    showSlides(1, 1);
+
+    function plusSlides(n, no) {
+        showSlides(slideIndex[no] += n, no);
+    }
+
+    function showSlides(n, no) {
+        let i;
+        let x = document.getElementsByClassName(slideId[no]);
+        if (n > x.length) { slideIndex[no] = 1 }
+        if (n < 1) { slideIndex[no] = x.length }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex[no] - 1].style.display = "block";
+    }
 });
